bcaf70694a1eb1224238626869beb9e49e659091
Christine <chrystyme123@gmail.com>
2022-11-08 21:31:15 -0800

    Work on fix for allowing new selections for each round

--- script.js
+++ script.js
@@ -4,65 +4,91 @@ let computerScore = 0;
 
 // Get computer choice: random number between 0, 1, 2, then assign to rock, paper, or scissors
 function getComputerChoice() {
-    const array = ["Rock", "Paper", "Scissors"];
+    const array = ["rock", "paper", "scissors"];
     let random = array[Math.floor(Math.random() * 3)];
     return random;
 }
 
+let computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+
 // Get player selection (case-insensitive)
-let playerInput = prompt("Rock, Paper, or Scissors?");
-if (playerInput.toLowerCase() == "rock") {
-    var playerSelection = "Rock";
-} else if (playerInput.toLowerCase() == "paper") {
-    var playerSelection = "Paper";
-} else if (playerInput.toLowerCase() == "scissors") {
-    var playerSelection = "Scissors";
-} else {
-    console.log('Invalid input. Please input "Rock," "Paper," or "Scissors"');
+function getPlayerChoice() {
+    let playerInput = prompt("Rock, Paper, or Scissors?");
+    if (playerInput.toLowerCase() == "rock") {
+        return "Rock";
+    } else if (playerInput.toLowerCase() == "paper") {
+        return "Paper";
+    } else if (playerInput.toLowerCase() == "scissors") {
+        return "Scissors";
+    } else {
+        console.log('Invalid input. Please input "Rock," "Paper," or "Scissors"');
+    }
 }
 
+let playerSelection = getPlayerChoice();
+
+console.log(playerSelection);
 
 // Play a single round of rock paper scissors
 function playRound(computerSelection, playerSelection) {
     // console.log("Computer: " + computerSelection + ", Player: " + playerSelection);
      if (computerSelection == playerSelection) {
-        console.log("Tie")
+        return "Tie";
      } else if (computerSelection == "Rock") {
         if (playerSelection == "Paper") {
             playerScore++;
+            return "Player wins";
         } else if (playerSelection == "Scissors") {
             computerScore++;
-        }
+        }   return "Computer wins";
      } else if (computerSelection == "Paper") {
         if (playerSelection == "Rock") {
             computerScore++;
+            return "Computer wins";
         } else if (playerSelection == "Scissors") {
             playerScore++;
+            return "Player wins";
         }
-     } else {
+     } else if (computerSelection == "Scissors") {
         if (playerSelection == "Rock") {
             playerScore++;
+            return "Player wins";
         } else if (playerSelection == "Paper") {
-            computerScore++
+            computerScore++;
+            return "Computer wins";
         }
      }
-     return playerScore;
-     return computerScore;
 }
 
+// playRound(computerSelection, playerSelection);
+
+// console.log(playerScore + "," + computerScore);
+
+
 // Play 5 rounds of rock paper scissors
-function game(computerSelection, playerSelection) {
+function game() {
     for (let i = 0; i < 5; i++) {
         playRound(computerSelection, playerSelection);
+        if (playRound() == "Tie") {
+            console.log("Tie");
+        } else if (playRound() == "Player wins") {
+            console.log("Player wins");
+        }
         console.log("Player: " + playerScore + ", Computer: " + computerScore);
         i++;
     }
 }
 
-game();
+game(computerSelection, playerSelection);
+
+
+
 
 
 // Compare player to computer, determine and print winner
+/* 
 if (playerScore > computerScore) {
     console.log("Player wins!");
 } else if (playerScore < computerScore) {
@@ -70,6 +96,7 @@ if (playerScore > computerScore) {
 } else {
     console.log("Tie!");
 }
+*/
 
 
 
