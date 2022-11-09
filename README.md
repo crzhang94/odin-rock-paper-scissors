572437cff9b3a4afcc5e43a09e7748ce4b53e1b1
Christine <chrystyme123@gmail.com>
2022-11-08 19:48:15 -0800

    Get computer selection

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # odin-rock-paper-scissors
 TOP Foundations: Rock Paper Scissors
+
+Simple version of rock paper scissors to be played in the browser console. 
---
+++ rockpaperscissors.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+// Get computer choice: random number between 0, 1, 2
+function getComputerChoice(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min );
+}
+
+// Get computer choice: assign random number to rock, paper, or scissors
+var computerNumber = getComputerChoice(0, 2)
+if (computerNumber = 0) {
+    var computerSelection = "Rock";
+} else if (computerNumber = 1) {
+    var computerSelection = "Paper";
+} else {
+    var computerSelection = "Scissors";
+}
+
+
+// Get player selection (case-insensitive)
+
+// function playRound
+// Play a single round of rock paper scissors
+
+// playerSelection
+// computerSelection
+// Compare player to computer, determine winner
+// Print winner
+
+
+
+
+
